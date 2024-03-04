42215854a2caec05442c4503a8acfe675d37ba30
devlim <ywl428@naver.com>
2024-03-04 15:56:23 +0900

    240304

--- C_basic.cpp
+++ C_basic.cpp
@@ -1,6 +1,6 @@
 #pragma warning(disable : 4996)
 #include<stdio.h>
-#include<conio.h> // getch();
+#include<conio.h>// getch();
 
 void main()
 {
@@ -55,7 +55,7 @@ RE:
 	printf("이름 입력 : "); scanf("%s", name);
 	printf("나이 입력 : "); scanf("%d", &age); getchar();
 	printf("혈액형 입력 : "); scanf("%c", &bloodType); getchar();와 같음 // == blood = getchar(); getchar();
-	
+
 	printf("내 이름은 %s이고, 나이는 %d살이며, 혈액형은 %c입니다!\n", name, age, bloodType);
 
 	printf("게속하시겠습니까?(Y/N) -> ");
@@ -68,6 +68,7 @@ RE:
 		return ;
 	*/
 
+	/*
 	char ch;
 	printf("한 문자 입력 : ");
 	ch = getchar(); // == scanf("%c", ch);
@@ -89,6 +90,98 @@ RE:
 	getch();
 
 	putchar(65 + 1);
+	putchardf('\n');
+	*/
+	
+	/*
+	char str1[100], str2[100];
+
+	printf("STR1 : "); gets_s(str1);
+	printf("STR2 : "); scanf("%s", str2);
+
+	printf("STR1 : %s\n", str1);
+	printf("STR2 : %s\n", str2);
+	getch();
+
+	puts(str1);
+	puts(str2);
+	getch();
+
+	puts("오늘은");
+	puts("토요일");
+	*/
+
+	/*
+	// 제어문자(\n 등)
+	printf("Date Error!\a");
+	puts("Date Error!\a");
+	printf("PSB - TV\b");
+	putchar(65);
 	putchar('\n');
+	printf("%d \t %d\n", 50, 100);
+	printf("%d \t %c\n", 65, 'A');
+	printf("%c \t %d\n", 65, 'A');
+	printf("%d \t %c\n", '\0', '\0');
+	*/
+	
+	/*
+	// 형식문자 출력 형태 정의하기(%d 등)
+	printf("%d\n", 123);
+	printf("%5d\n", 123);
+	printf("%05d\n", 123);
+	printf("%-5d\n", 123);
+
+	printf("%f\n", 123.45);
+	printf("%6.1f\n", 123.45);
+	printf("% .1f\n", 12.67); // "% .0f" >> 정수자리 수만 나옴
+
+	printf("%3c\n", 'A');
+	printf("%-3c\n", 'A');
+
+	printf("%10s\n", "ABCDEFG");
+	printf("%-10s\n", "ABCDEFG");
+	*/
+
+	/*
+	// 연산자
+	int a = 2, b = 2;
+	++a; // 전치(전위) 연산자
+	b++; // 후치(후위) 연산자
+	printf("%d %d\n", a, b);
+	
+	int a = 2, b = 2, c = -2, i, j, k;
+
+	i = ++a;
+	j = b++;
+	k = -c;
+
+	printf("%d %d\n", i, a);
+	printf("%d %d\n", j, b);
+	printf("%d %d\n", k, c);
+	*/
+	
+	/*
+	int a = 2, b = -3, c;
+
+	if (!(a + b))
+		c = 1;
+	else
+		c = 0;
+	printf("c = %d\n", c);
+	*/
+
+	/*
+	int a = 3, b, c, d, e;
+
+	e = (b = a + 3), c = b * 2, d = c - 2;
+	// d = c - 2, c = b * 2, e = (b = a + 3); >> b = 6, c = 쓰레기값, d = 쓰레기값, e = 6 
+
+	printf("b = %d\n", b);
+	printf("c = %d\n", c);
+	printf("d = %d\n", d);
+	printf("e = %d\n", e);
+	*/
+
+	
 
 }
